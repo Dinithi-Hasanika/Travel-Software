77ed707eaf105af5d9299d506555f94272e128ad
Dinithi Hasanika <dinithiliyanage.95@gmail.com>
2021-05-24 16:43:46 +0530

    routes added

--- travel-front/src/app/app-routing.module.ts
+++ travel-front/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CallCenterComponent} from './call-center/call-center.component';
 import {HomeComponent} from './home/home.component';
+import {ContractAdderComponent} from './contract-adder/contract-adder.component';
+import {ContractDeleteComponent} from './contract-delete/contract-delete.component';
+import {ContractUpdateComponent} from './contract-update/contract-update.component';
+import {ContractViewComponent} from './contract-view/contract-view.component';
 
 const routes: Routes = [
   { path: "home/call-center", component: CallCenterComponent },
   { path: "home", component: HomeComponent },
+  {path: "home/contract-adder",component: ContractAdderComponent},
+  {path: "home/contract-delete",component: ContractDeleteComponent},
+  {path: "home/contract-update",component: ContractUpdateComponent},
+  {path: "home/contract-view",component: ContractViewComponent},
   { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
 
